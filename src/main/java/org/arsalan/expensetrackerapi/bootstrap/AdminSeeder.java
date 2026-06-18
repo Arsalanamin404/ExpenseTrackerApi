3571f966df6a450e9f5df5396488639f81efb49c
Arslanamin404 <arsalanrather.dev@gmail.com>
2026-06-18 10:35:30 +0530

    ADD .env to project

--- src/main/java/org/arsalan/expensetrackerapi/bootstrap/AdminSeeder.java
+++ src/main/java/org/arsalan/expensetrackerapi/bootstrap/AdminSeeder.java
@@ -3,12 +3,22 @@ package org.arsalan.expensetrackerapi.bootstrap;
 import org.arsalan.expensetrackerapi.auth.entity.User;
 import org.arsalan.expensetrackerapi.auth.repository.UserRepository;
 import org.arsalan.expensetrackerapi.common.enums.Role;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AdminSeeder implements CommandLineRunner {
+    @Value("${admin.fullName}")
+    private String adminFullName;
+
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -19,12 +29,13 @@ public class AdminSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        if (userRepository.findByEmail("admin@gmail.com").isEmpty()){
+
+        if (userRepository.findByEmail(adminEmail).isEmpty()){
             User admin = new User();
-            admin.setFullName("SYSTEM ADMIN");
-            admin.setEmail("admin@gmail.com");
+            admin.setFullName(adminFullName);
+            admin.setEmail(adminEmail);
             admin.setPassword(
-                    passwordEncoder.encode("admin@123")
+                    passwordEncoder.encode(adminPassword)
             );
 
             admin.setRole(Role.ADMIN);
